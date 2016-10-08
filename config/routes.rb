f52c09d2d95666e2918099e98db1d05b382baca0
Brandon Lam <brandonywlam@gmail.com>
2016-10-08 14:22:11 -0700

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
   resources :providers, only: [:new, :create]
+
   resources :customers, only: [:new, :create]
   root "statics#index"
 end
