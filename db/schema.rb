10e1fd0244709cfe3bed5f8ee33039e6c0d109cf
Brandon Lam <brandonywlam@gmail.com>
2016-10-19 23:25:54 -0700

    style bookings index, provider index, services index/show

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20161011221738) do
   end
 
   create_table "customers", force: :cascade do |t|
-    t.datetime "created_at" ,null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
